08a5062ea2cafb8ca7f1274ebf93e0e228b98f26
fo0d <sputnik.y3@gmail.com>
2021-05-03 16:12:42 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/dbgspy_demo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../build/dbgspy_demo"
+  "../build/dbgspy_demo.pdb"
+  "CMakeFiles/dbgspy_demo.dir/dbgspy_demo.c.o"
+  "CMakeFiles/dbgspy_demo.dir/home/binary0x01/area51/y3libgen/dbgspy.c.o"
+  "CMakeFiles/dbgspy_demo.dir/home/binary0x01/area51/y3libgen/hash.c.o"
+  "CMakeFiles/dbgspy_demo.dir/home/binary0x01/area51/y3libgen/list.c.o"
+  "CMakeFiles/dbgspy_demo.dir/home/binary0x01/area51/y3libgen/primes.c.o"
+  "CMakeFiles/dbgspy_demo.dir/home/binary0x01/area51/y3libgen/str.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dbgspy_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/compile.h
@@ -0,0 +1,33 @@
+/* ********************************************************* *
+* By Yuriy Y. Yermilov aka (binaryONE) cyclone.yyy@gmail.com
+*
+* website: code.computronium.io
+*
+* THIS SOFTWARE IS PROVIDED BY THE OWNER ``AS IS''
+* AND ANY EXPRESS OR IMPLIED WARRANTIES,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE OWNER
+* PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* ********************************************************** */
+
+//
+// Order matters.
+//
+
+void y3_dbgspy(struct y3_dbg *T);
+
+void run_dbgspy_default_test();
+void run_fib_test();
+void fib_viz_output(struct y3_dbg *T, struct y3_dbg_target *my_targets[]);
+void run_dbgspy_fib_test();
